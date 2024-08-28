e5f0dcef48d60347bdaf9dda322705fd4e0f7b06
Jonayet <137201209+Jonayet10@users.noreply.github.com>
2024-08-28 11:42:18 -0400

    Update heap.c

--- src/heap.c
+++ src/heap.c
@@ -2,6 +2,13 @@
 
 #include <stdlib.h>
 
+/**
+ * @brief A structure representing a dynamic heap.
+ * 
+ * This structure manages a dynamic array of pointers, allowing the addition
+ * and retrieval of elements. The array is dynamically resized as new elements 
+ * are added.
+ */
 typedef struct heap {
     /** Generic array of pointers. */
     int32_t **ptr;
@@ -9,6 +16,14 @@ typedef struct heap {
     int32_t count;
 } heap_t;
 
+/**
+ * @brief Initializes a new heap.
+ * 
+ * This function allocates memory for a new heap structure and initializes 
+ * the pointer array to NULL and the count to zero.
+ * 
+ * @return A pointer to the initialized heap structure.
+ */
 heap_t *heap_init() {
     // The heap array is initially allocated to hold zero elements.
     heap_t *heap = malloc(sizeof(heap_t));
@@ -17,6 +32,17 @@ heap_t *heap_init() {
     return heap;
 }
 
+/**
+ * @brief Adds a new pointer to the heap.
+ * 
+ * This function resizes the internal pointer array to accommodate the new 
+ * element, adds the new pointer to the end of the array, and increments the 
+ * element count.
+ * 
+ * @param heap A pointer to the heap structure.
+ * @param ptr The pointer to be added to the heap.
+ * @return The index at which the new pointer was added.
+ */
 int32_t heap_add(heap_t *heap, int32_t *ptr) {
     heap->ptr = realloc(heap->ptr, (heap->count + 1) * sizeof(int32_t *));
     heap->ptr[heap->count] = ptr;
@@ -25,10 +51,27 @@ int32_t heap_add(heap_t *heap, int32_t *ptr) {
     return temp;
 }
 
+/**
+ * @brief Retrieves a pointer from the heap by its index.
+ * 
+ * This function returns the pointer stored at the specified index in the heap.
+ * 
+ * @param heap A pointer to the heap structure.
+ * @param ref The index of the pointer to retrieve.
+ * @return The pointer stored at the specified index.
+ */
 int32_t *heap_get(heap_t *heap, int32_t ref) {
     return heap->ptr[ref];
 }
 
+/**
+ * @brief Frees the memory allocated for the heap.
+ * 
+ * This function frees all pointers stored in the heap as well as the internal
+ * pointer array and the heap structure itself.
+ * 
+ * @param heap A pointer to the heap structure to be freed.
+ */
 void heap_free(heap_t *heap) {
     for (int32_t i = 0; i < heap->count; i++) {
         free(heap->ptr[i]);
